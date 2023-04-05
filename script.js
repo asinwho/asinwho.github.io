71f09e5a35a75107847d392510cf4cb288eb34aa
En(d) <61750489+hebephoh@users.noreply.github.com>
2023-04-05 19:11:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let container = document.getElementById('container');
+let reptText = document.getElementById('repeating-text');
+let eyeContainer = document.getElementById('eye-container');
+let pupil = document.getElementById('pupil');
+
+function followCursor(event) {
+ let x = event.clientX;
+ let y = event.clientY;
+ let centerHor = window.innerWidth / 2;
+ let centerVert = window.innerHeight / 2;
+ let xNormalized = (x - centerHor) * 3;
+ let yNormalized = (y - centerVert) * 3;
+ container.style.setProperty('--x', xNormalized + 'px');
+ container.style.setProperty('--y', yNormalized + 'px');
+}
+
+document.addEventListener('mousemove', followCursor);
+
+let container1 = document.getElementById('container');
+let text = document.getElementById('text');
+let newText = 'be not afraid be not afraid be not afraid be not afraid be not afraid be not afraid';
+
+function addText() {
+ newText = ' ' + newText;
+ text.innerText += newText;
+}
+
+setInterval(addText, 1000);
